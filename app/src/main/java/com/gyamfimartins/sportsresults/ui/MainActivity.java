6cedaec52b9cb71d0e2022cea7127148d6a46cbb
gyamfimartins <44516295+gyamfimartins@users.noreply.github.com>
2021-03-02 00:39:52 -0500

    updated MainActivity.java

--- app/src/main/java/com/gyamfimartins/sportsresults/ui/MainActivity.java
+++ app/src/main/java/com/gyamfimartins/sportsresults/ui/MainActivity.java
@@ -13,11 +13,8 @@ import com.gyamfimartins.sportsresults.R;
 
 
 public class MainActivity extends AppCompatActivity {
-
     private ProgressBar progress_circular;
-    private int progressBarStatus = 0;
-    private long fileSize = 0;
-    private final Handler progressBarHandler = new Handler();
+    private Button btn_loaddata;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,60 +23,28 @@ public class MainActivity extends AppCompatActivity {
 
         progress_circular = findViewById(R.id.progress_circular);
 
-        Button btn_loaddata = findViewById(R.id.btn_loaddata);
+        btn_loaddata = findViewById(R.id.btn_loaddata);
         btn_loaddata.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                btn_loaddata.setVisibility(View.GONE);
-                progress_circular.setVisibility(View.VISIBLE);
-                showprogress();
+                delay();
             }
         });
 
     }
 
 
-    private void showprogress() {
-        new Thread(new Runnable() {
+    private void delay() {
+        btn_loaddata.setVisibility(View.GONE);
+        progress_circular.setVisibility(View.VISIBLE);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
             public void run() {
-                while (progressBarStatus < 100) {
-                    progressBarStatus = doSomeTasks();
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    progressBarHandler.post(new Runnable() {
-                        public void run() {
-                            progress_circular.setProgress(progressBarStatus);
-                        }
-                    });
-                }
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                Intent intent = new Intent(MainActivity.this, DisplayDataActivity.class);
-                startActivity(intent);
+                startActivity(new Intent(MainActivity.this, DisplayDataActivity.class));
+                finish();
             }
-        }).start();
-
-    }
-
-
-    public int doSomeTasks() {
-        while (fileSize <= 1000000) {
-            fileSize++;
-            if (fileSize == 100000) {
-                return 10;
-            } else if (fileSize == 200000) {
-                return 20;
-            } else if (fileSize == 300000) {
-                return 30;
-            }
-        }
-        return 100;
+        }, 5000);
     }
 
 
